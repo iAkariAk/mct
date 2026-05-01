e0932b37ef58cee5f619d0ca92737ba59aa0eea6
Akari <akkarinn@qq.com>
2026-05-01 10:23:09 +0800

    Fix

--- util/src/commonMain/kotlin/mct/util/translator/Util.kt
+++ util/src/commonMain/kotlin/mct/util/translator/Util.kt
@@ -4,14 +4,14 @@ internal const val TOKEN_COUNT_THRESHOLD = 100 shl 10 // 100k
 
 internal expect fun calculateToken(str: String): Int
 
-internal fun List<String>.chunkedByToken(tokenSizePerChunk: Int): Sequence<MutableList<String>> {
+internal fun List<String>.chunkedByToken(tokenSizePerChunk: Int = TOKEN_COUNT_THRESHOLD): Sequence<MutableList<String>> {
     val sources = this
     var tokenCount = 0
     return sequence {
         val tmp = mutableListOf<String>()
         sources.forEachIndexed { index, source ->
             val approximateTokenCount = calculateToken(source)
-            val isThresholdOver = tokenCount + approximateTokenCount >= TOKEN_COUNT_THRESHOLD
+            val isThresholdOver = tokenCount + approximateTokenCount >= tokenSizePerChunk
             if (isThresholdOver) {
                 require(tmp.isNotEmpty()) { "The content size too large." }
                 yield(tmp)
