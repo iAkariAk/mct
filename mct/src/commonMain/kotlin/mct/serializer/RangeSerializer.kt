07aff54de7a25a0010d673d7402ea6fb5286f252
Akari <akkarinn@qq.com>
2026-09-16 18:32:18 +0800

    Use array to represent IntArray

--- mct/src/commonMain/kotlin/mct/serializer/RangeSerializer.kt
+++ mct/src/commonMain/kotlin/mct/serializer/RangeSerializer.kt
@@ -2,21 +2,20 @@ package mct.serializer
 
 import kotlinx.serialization.KSerializer
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.builtins.IntArraySerializer
+import kotlinx.serialization.descriptors.SerialDescriptor
 import kotlinx.serialization.encoding.Decoder
 import kotlinx.serialization.encoding.Encoder
 
-@Serializable
-private data class IntRangeDelegate(val startInclusive: Int, val endInclusive: Int)
-
 object IntRangeSerializer : KSerializer<IntRange> {
-    private val delegated = IntRangeDelegate.serializer()
-    override val descriptor get() = delegated.descriptor
+    private val delegated = IntArraySerializer()
+    override val descriptor = SerialDescriptor("mct.serializer.IntRangeSerializer", delegated.descriptor)
 
     override fun serialize(encoder: Encoder, value: IntRange) =
-        delegated.serialize(encoder, IntRangeDelegate(value.first, value.last))
+        delegated.serialize(encoder, intArrayOf(value.first, value.last))
 
     override fun deserialize(decoder: Decoder): IntRange =
-        delegated.deserialize(decoder).run { startInclusive..endInclusive }
+        delegated.deserialize(decoder).let { (startInclusive, endInclusive) -> IntRange(startInclusive, endInclusive) }
 }
 
 typealias IntRangeSerializable = @Serializable(IntRangeSerializer::class) IntRange
